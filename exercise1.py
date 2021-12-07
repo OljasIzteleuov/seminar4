2e71cb1950e2a3c970a3162564af6e6cee334085
Olzhas Izteleuov <92367536+OljasIzteleuov@users.noreply.github.com>
2021-12-07 00:50:40 -0500

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -1,8 +1,5 @@
-a=int(input("Введите число:"))
-
-def exe1(n1):
-      if (a>n1) or (a<-n1):
-            print("a=",a*0)
-      else:
-            print("a=",a+1)
-exe1(100)
+a=int(input("Санды жаз = "))
+if a>100 or a<-100:
+        print("0")
+else:
+        print("1")
